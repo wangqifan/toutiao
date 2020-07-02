a69254363f9730c0011a43cd168023ea73b202da
wangqifan <1694675518@qq.com>
2020-07-02 16:11:43 +0800

    edit

--- src/main/java/com/wangqifan/toutiao/Service/QiniuService.java
+++ src/main/java/com/wangqifan/toutiao/Service/QiniuService.java
@@ -1,6 +1,7 @@
 package com.wangqifan.toutiao.Service;
 
 
+import com.qiniu.common.Zone;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.wangqifan.toutiao.util.ToutiaoUtil;
@@ -25,7 +26,7 @@ public class QiniuService {
     String bucketname = "wqf";
 
     //构造一个带指定 Region 对象的配置类
-    Configuration cfg = new Configuration(Region.region0());
+    Configuration cfg = new Configuration(Zone.zone2());
 
     //密钥配置
     Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
